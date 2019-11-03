8fc581921205e412d97ba0f312c2d46291c91609
youness el housni <elhousni.yns@gmail.com>
2019-11-03 04:46:38 +0100

    verification function to enable start button

--- src/Components/Fonctions.js
+++ src/Components/Fonctions.js
@@ -3,9 +3,11 @@ export function validateChange(data) {
   if (data.length > 1) {
     // case of changing value of an input
     return parseInt(data.substr(1, 2), 10);
-  } else {
+  } else if (data.length === 1) {
     // initiale valid value
     return parseInt(data.substr(0, 1), 10);
+  } else {
+    return null;
   }
 }
 export function verifyChange(data) {
@@ -26,4 +28,27 @@ export function verifyChange(data) {
     }
   }
   return true;
+}
+
+export function validate(startObject) {
+  let validationTable = [null, 1, 2, 3, 4, 5, 6, 7, 8];
+  for (let value in startObject) {
+    if (validationTable.includes(startObject[value])) {
+      // we verify that value is included in table
+
+      const index = validationTable.findIndex(validationElement => {
+        return validationElement === startObject[value]; // return index of value included
+      });
+      //console.log(index);
+      validationTable.splice(index, 1);
+      // delete that element to avoid repitition
+    } else {
+      return true;
+    }
+  }
+  if (validationTable.length > 0) {
+    return true; // the table still include some values so it ain't valid
+  }
+  //console.log(validationTable);
+  return false; // validation is a must button will be clickable now
 }
--- src/Components/View.js
+++ src/Components/View.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import TableStyle from "../Style/Table.css";
-import { validateChange, verifyChange } from "./Fonctions";
+import { validateChange, verifyChange, validate } from "./Fonctions";
 
 export default function View() {
   const [startObject, setStartObject] = useState({
@@ -158,90 +158,94 @@ export default function View() {
           </div>
         )}
         <div style={{ padding: "auto", margin: "auto", paddingTop: 30 }}>
-          <button className="btn btn-danger">start the fun</button>
+          <button className="btn btn-danger" disabled={validate(startObject)}>
+            start the fun
+          </button>
         </div>
       </div>
 
       <table>
         <tbody>
-          <td>
-            <table>
-              <tbody>
-                <tr>
-                  <td>{T1[0][0]}</td>
-                  <td>{T1[0][1]}</td>
-                  <td>{T1[0][2]}</td>
-                </tr>
-                <tr>
-                  <td>{T1[1][0]}</td>
-                  <td>{T1[1][1]}</td>
-                  <td>{T1[1][2]}</td>
-                </tr>
-                <tr>
-                  <td>{T1[2][0]}</td>
-                  <td>{T1[2][1]}</td>
-                  <td>{T1[2][2]}</td>
-                </tr>
-              </tbody>
-            </table>
-            <table>
-              <tbody>
-                <tr>
-                  <td>{T2[0][0]}</td>
-                  <td>{T2[0][1]}</td>
-                  <td>{T2[0][2]}</td>
-                </tr>
-                <tr>
-                  <td>{T2[1][0]}</td>
-                  <td>{T2[1][1]}</td>
-                  <td>{T2[1][2]}</td>
-                </tr>
-                <tr>
-                  <td>{T2[2][0]}</td>
-                  <td>{T2[2][1]}</td>
-                  <td>{T2[2][2]}</td>
-                </tr>
-              </tbody>
-            </table>
-            <table>
-              <tbody>
-                <tr>
-                  <td>{T3[0][0]}</td>
-                  <td>{T3[0][1]}</td>
-                  <td>{T3[0][2]}</td>
-                </tr>
-                <tr>
-                  <td>{T3[1][0]}</td>
-                  <td>{T3[1][1]}</td>
-                  <td>{T3[1][2]}</td>
-                </tr>
-                <tr>
-                  <td>{T3[2][0]}</td>
-                  <td>{T3[2][1]}</td>
-                  <td>{T3[2][2]}</td>
-                </tr>
-              </tbody>
-            </table>
-            <table>
-              <tbody>
-                <tr>
-                  <td>{T4[0][0]}</td>
-                  <td>{T4[0][1]}</td>
-                  <td>{T4[0][2]}</td>
-                </tr>
-                <tr>
-                  <td>{T4[1][0]}</td>
-                  <td>{T4[1][1]}</td>
-                  <td>{T4[1][2]}</td>
-                </tr>
-                <tr>
-                  <td>{T4[2][0]}</td>
-                  <td>{T4[2][1]}</td>
-                  <td>{T4[2][2]}</td>
-                </tr>
-              </tbody>
-            </table>
-          </td>
+          <tr>
+            <td>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>{T1[0][0]}</td>
+                    <td>{T1[0][1]}</td>
+                    <td>{T1[0][2]}</td>
+                  </tr>
+                  <tr>
+                    <td>{T1[1][0]}</td>
+                    <td>{T1[1][1]}</td>
+                    <td>{T1[1][2]}</td>
+                  </tr>
+                  <tr>
+                    <td>{T1[2][0]}</td>
+                    <td>{T1[2][1]}</td>
+                    <td>{T1[2][2]}</td>
+                  </tr>
+                </tbody>
+              </table>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>{T2[0][0]}</td>
+                    <td>{T2[0][1]}</td>
+                    <td>{T2[0][2]}</td>
+                  </tr>
+                  <tr>
+                    <td>{T2[1][0]}</td>
+                    <td>{T2[1][1]}</td>
+                    <td>{T2[1][2]}</td>
+                  </tr>
+                  <tr>
+                    <td>{T2[2][0]}</td>
+                    <td>{T2[2][1]}</td>
+                    <td>{T2[2][2]}</td>
+                  </tr>
+                </tbody>
+              </table>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>{T3[0][0]}</td>
+                    <td>{T3[0][1]}</td>
+                    <td>{T3[0][2]}</td>
+                  </tr>
+                  <tr>
+                    <td>{T3[1][0]}</td>
+                    <td>{T3[1][1]}</td>
+                    <td>{T3[1][2]}</td>
+                  </tr>
+                  <tr>
+                    <td>{T3[2][0]}</td>
+                    <td>{T3[2][1]}</td>
+                    <td>{T3[2][2]}</td>
+                  </tr>
+                </tbody>
+              </table>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>{T4[0][0]}</td>
+                    <td>{T4[0][1]}</td>
+                    <td>{T4[0][2]}</td>
+                  </tr>
+                  <tr>
+                    <td>{T4[1][0]}</td>
+                    <td>{T4[1][1]}</td>
+                    <td>{T4[1][2]}</td>
+                  </tr>
+                  <tr>
+                    <td>{T4[2][0]}</td>
+                    <td>{T4[2][1]}</td>
+                    <td>{T4[2][2]}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </td>
+          </tr>
         </tbody>
       </table>
     </div>
